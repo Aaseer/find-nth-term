73ea4dfef4f8a601e552f3fe9a176982dc85b3ca
Aaseer <aaseerahbab@gmail.com>
2021-09-19 22:26:21 +0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # find nth term
- This is a progra to help those who cant do multiplication for finding the nth term. This is basically a cheat program lol :)
+ This is a simple python program to help those who cant do multiplication for finding the nth term. This is basically a cheat program lol :)
+This a simple python program which has no copyright :)
+            -Aaseer
---
+++ main.py
@@ -0,0 +1,16 @@
+print('This program finds the n term of an arithmetic sequence')
+numbers = str(input('Give me the first 3 numbers of the sequence (split them with a ", "): '))
+n_term = int(input('The term you wanna find: '))
+
+pattern = numbers.split(', ')
+pattern_list = list(map(int, pattern))
+
+
+
+if pattern_list[0] - pattern_list[1] == pattern_list[1] - pattern_list[2]: # SUBTRACTION ARITHMATIC
+    print(pattern_list[0] + (n_term - 1) * (pattern_list[1] - pattern_list[0]))
+
+elif pattern_list[0] + pattern_list[1] == pattern_list[1] + pattern_list[2]: # ADDITION ARITHMATIC
+    print(pattern_list[0] + (n_term - 1) * (pattern_list[1] + pattern_list[0]))
+else:
+    print('That is not an arithmatic sequence')
