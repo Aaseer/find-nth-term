2823d8bd7f3cb123de8c9021e18afa8cec2ea1de
Aaseer <79782343+Aaseer@users.noreply.github.com>
2021-09-20 12:08:49 +0600

    Fixed some spelling errors

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ pattern = numbers.split(', ')
 pattern_list = list(map(int, pattern))
 
 
-
 if pattern_list[0] - pattern_list[1] == pattern_list[1] - pattern_list[2]: # SUBTRACTION ARITHMATIC
     print(pattern_list[0] + (n_term - 1) * (pattern_list[1] - pattern_list[0]))
 
 elif pattern_list[0] + pattern_list[1] == pattern_list[1] + pattern_list[2]: # ADDITION ARITHMATIC
     print(pattern_list[0] + (n_term - 1) * (pattern_list[1] + pattern_list[0]))
 else:
-    print('That is not an arithmatic sequence')
+    print('That is not an arithmetic sequence')
